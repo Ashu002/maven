d834752bbe64f03b19d8b35b0ccfd8ed69a31bd7
Ashu002 <ashu.ojha2010@gmail.com>
2016-08-16 17:20:53 +0530

    new changes Again1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,17 @@
  * Created by jft on 12/8/16.
  */
 public class Test {
+
+    public static void t1st() {
+        t1st();
+        System.out.println("oooo1ssooooooooooo");
+    }
+    public static void test() {
+        System.out.println("ooooooooooooooo");
+    }
     public static void main(String ar[]){
+        test();
         System.out.println("This is the Starting q24f Maven ok");
     }
+
 }
