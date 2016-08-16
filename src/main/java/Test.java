d13b6ee8debdb403a8b9c83a9841b654b95f6d15
Ashu002 <ashu.ojha2010@gmail.com>
2016-08-16 17:18:25 +0530

    Test Directory changes

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,15 @@
  * Created by jft on 12/8/16.
  */
 public class Test {
+    public static void test() {
+        System.out.println("ooooooooooooooo");
+    }
+    public static void t1st() {
+        System.out.println("oooo1ssooooooooooo");
+    }
     public static void main(String ar[]){
+        test();
+        t1st();
         System.out.println("This is the Starting q24f Maven ok");
     }
 }
